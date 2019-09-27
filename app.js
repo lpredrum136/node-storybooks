3c267fbc7c528d7b1014f27c58a9e92240cdab6f
Henry Nguyen <nguyenhai1591@gmail.com>
2019-09-27 15:53:00 +1000

    fix port I am so dumb

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('It works');
 })
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
