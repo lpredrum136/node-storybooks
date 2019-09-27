9375e4b243c1201fd8b6db6ae84107d1bccbba7a
Henry Nguyen <nguyenhai1591@gmail.com>
2019-09-27 20:27:01 +1000

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Henry:legolas136@storybooks-dev-roq1b.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID: '839999018490-m7946s3kn1j53rplpli7qqn4v5lenr1k.apps.googleusercontent.com',
+    googleClientSecret: 'lL91COURbqvRCYGSv7E8_UOq'
+};
